73e5ad6df12210e089743211f15b3bfa17ae071e
rStar <starlove1247@gmail.com>
2025-04-24 13:32:17 +0800

    處理斜著走比較快的問題，將移動方向正規化

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -14,6 +14,8 @@ public class CharacterController : MonoBehaviour
         // 垂直方向
         var vertical  = Input.GetAxisRaw("Vertical");
         var direction = new Vector3(horizontal , vertical , 0); // 移動方向
+        // 處理斜著走比較快的問題，將移動方向正規化
+        direction = direction.normalized;
         // Time.deltaTime = 1/fps 抵銷FPS的影響
         transform.position += direction * moveSpeed * Time.deltaTime;
     }
