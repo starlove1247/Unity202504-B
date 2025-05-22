51b75ee28b3ee0448ecec28e9c527ea18a546c7e
rStar <starlove1247@gmail.com>
2025-05-22 13:31:43 +0800

    重構對話系統，將大部分功能移動到對話系統身上

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Globalization;
 using NaughtyAttributes;
 using TMPEffects.Components;
 using UnityEngine;
@@ -7,6 +9,11 @@ public class Dialog : MonoBehaviour
     [SerializeField]
     private TMPWriter tmpWriter;
 
+    /// <summary>
+    /// 目前的對話資料Index，從0開始
+    /// </summary>
+    private int dialogIndex = 0;
+    private List<string> dialogTexts;
     [Button("播放打字機效果")]
     public void PlayWriter()
     {
@@ -22,5 +29,43 @@ public class Dialog : MonoBehaviour
     public void SetText(string dialogText)
     {
        tmpWriter.SetText(dialogText); 
+    }
+
+    public void PlayFirstDialog()
+    {
+        // 沒有任何對話資料，不做任何事情
+        if (dialogTexts.Count == 0)
+        {
+            Debug.LogError("錯誤，對話資料鎮列為空");
+            return;
+        }
+
+        dialogIndex = 0; // 重置Index
+        var dialogText = dialogTexts[dialogIndex];
+        SetText(dialogText);
+        PlayWriter(); 
+    }
+
+    public void SetDialogTexts(List<string> texts)
+    {
+        dialogTexts = texts;
+    }
+
+    public void PlayNextDialog()
+    {
+        // 沒有任何對話資料，不做任何事情
+        if (dialogTexts.Count == 0)
+        {
+            Debug.LogError("錯誤，對話資料鎮列為空");
+            return;
+        }
+
+        // 沒有下一段話，就不做任何事情
+        if (dialogIndex + 1 == dialogTexts.Count) return;
+
+        dialogIndex++;
+        var dialogText = dialogTexts[dialogIndex];
+        SetText(dialogText);
+        PlayWriter(); 
     }
 }
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -9,43 +9,19 @@ public class NPC : MonoBehaviour
     [SerializeField]
     private Dialog dialog;
 
-    /// <summary>
-    /// 目前的對話資料Index，從0開始
-    /// </summary>
-    private int dialogIndex = 0;
+  
 
     [Button("顯示第一段話")]
     public void PlayFirstDialog()
     {
-        // 沒有任何對話資料，不做任何事情
-        if (dialogData.dialogTexts.Count == 0)
-        {
-            Debug.LogError("錯誤，對話資料鎮列為空");
-            return;
-        }
-
-        dialogIndex = 0; // 重置Index
-        var dialogText = dialogData.dialogTexts[dialogIndex];
-        dialog.SetText(dialogText);
-        dialog.PlayWriter();
+        dialog.SetDialogTexts(dialogData.dialogTexts);
+        dialog.PlayFirstDialog();
     }
 
     [Button("顯示下一段話")]
     public void PlayNextDialog()
     {
-        // 沒有任何對話資料，不做任何事情
-        if (dialogData.dialogTexts.Count == 0)
-        {
-            Debug.LogError("錯誤，對話資料鎮列為空");
-            return;
-        }
-        // 沒有下一段話，就不做任何事情
-        if (dialogIndex + 1 == dialogData.dialogTexts.Count) return;
-
-        dialogIndex++;
-        var dialogText = dialogData.dialogTexts[dialogIndex];
-        dialog.SetText(dialogText);
-        dialog.PlayWriter();
+        dialog.PlayNextDialog();
     }
 
     [Button("Skip對話")]
